83240bde57446278c51e85a8b227811c98553f99
Peter Steinberger <steipete@gmail.com>
2026-01-09 22:32:42 +0100

    refactor: move WhatsApp web login methods into plugin

--- src/gateway/server.ts
+++ src/gateway/server.ts
@@ -274,8 +274,6 @@ const BASE_METHODS = [
   "send",
   "agent",
   "agent.wait",
-  "web.login.start",
-  "web.login.wait",
   // WebChat WebSocket-native chat methods
   "chat.history",
   "chat.abort",
